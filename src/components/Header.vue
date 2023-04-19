ee39121fe960a6871665832c667af8f3678d7a87
Luan Nguyen <luan.ndd.60cntt@ntu.edu.vn>
2023-04-20 00:31:33 +0700

    no message

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header-bar">
+    <div class="header-logo">
+      <img src="../assets/img/website-logo.svg" alt="" />
+    </div>
+    <div class="header-function">
+        <div class="notify"><font-awesome-icon :icon="['fas', 'bell']" size="xl" style="color: #755c5c;" /></div>
+        <div class="shopping-cart"><font-awesome-icon :icon="['fas', 'cart-shopping']" size="xl" style="color: #5c626a;" /></div>
+        <div class="user-avatar"><font-awesome-icon :icon="['fas', 'user']" size="xl" style="color: #404754;" /></div>
+    </div>
+  </div>
+</template>
+
+<style>
+    @import url("../assets/css/Header.css");
+</style>
---
+++ src/view/homepage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="section-header-navbar">
+        <div class="container">
+            
+        </div>
+    </div>
+</template>
